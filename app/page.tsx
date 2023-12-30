6572d2fc7904c7a22d87d5e8aa456d8117773347
hclanka <152237163+chandudagoat@users.noreply.github.com>
2023-12-30 21:32:56 +0530

    added dark mode and light mode and playing with shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
   );
 }
