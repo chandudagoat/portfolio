22a14ffc103edc114e65197e35a1d04e894af1ad
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-03-22 18:23:43 +0530

    done with website finally

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,22 @@
+import BlogPost from "@/components/blogpost";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
 export default function Blog() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <Navbar />
 
       <h1 className="text-2xl text-center font-bold my-10">Posts</h1>
+
+      <div className="mb-4">
+        <BlogPost
+          href="/cursorvsvscode"
+          title="Cursor vs Vscode"
+          date="22nd March, 2024"
+        />
+      </div>
+
       <Footer />
     </div>
   );
