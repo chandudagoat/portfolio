fb90d70952bde15be66b30f1c24ad2a546f7db70
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-03-22 18:21:22 +0530

    added blog post card in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Projects from "@/components/projects";
+import BlogPost from "@/components/blogpost";
 
 export default function Home() {
   return (
@@ -10,10 +11,17 @@ export default function Home() {
       <Navbar />
       <div className="my-10 flex flex-col items-center justify-center">
         <a href="/blog">
-          <h1 className="font-semibold underline decoration mb-2">
+          <h1 className="font-semibold underline decoration mb-4">
             blog posts
           </h1>
         </a>
+        <div className="mb-8">
+          <BlogPost
+            href="/cursorvsvscode"
+            title="Cursor vs Vscode"
+            date="22nd March, 2024"
+          />
+        </div>
 
         <h1 className="text-center font-semibold">&quot;projects&quot;</h1>
         <Projects />
---
+++ components/blogpost.tsx
@@ -0,0 +1,18 @@
+import { Card } from "./ui/card";
+
+interface Props {
+  title: string;
+  date: string;
+  href: string;
+}
+
+export default function BlogPost(props: Props) {
+  return (
+    <a href={props.href}>
+      <Card className="w-[500px] p-4 flex items-center transition ease-in justify-between hover:bg-gray-900">
+        <p>{props.title}</p>
+        <p>{props.date}</p>
+      </Card>
+    </a>
+  );
+}
