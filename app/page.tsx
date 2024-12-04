9c9366be55a600628a9b303d225a26fc49a6a411
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-12-04 10:50:22 +0530

    added playlists

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,41 @@ export default function Home() {
           />
         </div>
 
-        <h1 className="text-center font-semibold">&quot;projects&quot;</h1>
-        <Projects />
+        <h1 className="text-center font-semibold">playlists</h1>
+        <div className="w-fit my-10 grid grid-cols-2 gap-4">
+          <iframe
+            className="rounded-[12px]"
+            src="https://open.spotify.com/embed/playlist/6NQqHRDO1VkJpCPdnbsnSk?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+          <iframe
+            className="rounded-[12px]"
+            src="https://open.spotify.com/embed/playlist/1v88zYLI67z9C03NJ1qSEz?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+          <iframe
+            className="rounded-[12px]"
+            src="https://open.spotify.com/embed/playlist/0Fw7LRShvscnv6aGNKxskp?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+          <iframe
+            className="rounded-[12px]"
+            src="https://open.spotify.com/embed/playlist/4eIY8Z4MCDdhZskFwnUn8k?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
       <Footer />
     </div>
