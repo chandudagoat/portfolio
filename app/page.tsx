5ecafcddac0600c47327ce15173ec30284d03ebf
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-01-17 15:53:30 +0530

    added projects back

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,11 @@ export default function Home() {
           />
         </div>
 
+        <h1 className="text-center font-semibold">projects</h1>
+        <div className="mb-10">
+          <Projects />
+        </div>
+
         <h1 className="text-center font-semibold">playlists</h1>
         <div className="w-fit my-10 grid grid-cols-2 gap-4">
           <iframe
@@ -41,24 +46,9 @@ export default function Home() {
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
           ></iframe>
-          <iframe
-            className="rounded-[12px]"
-            src="https://open.spotify.com/embed/playlist/0Fw7LRShvscnv6aGNKxskp?utm_source=generator&theme=0"
-            width="100%"
-            height="352"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          ></iframe>
-          <iframe
-            className="rounded-[12px]"
-            src="https://open.spotify.com/embed/playlist/4eIY8Z4MCDdhZskFwnUn8k?utm_source=generator&theme=0"
-            width="100%"
-            height="352"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          ></iframe>
         </div>
       </div>
+
       <Footer />
     </div>
   );
