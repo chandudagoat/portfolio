b0d736ce0b3ecdcd390de9cbf69c7aaf233ecd06
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-01-17 15:54:58 +0530

    tweaking

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,12 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Projects from "@/components/projects";
-import BlogPost from "@/components/blogpost";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <div className="my-10 flex flex-col items-center justify-center">
-        <a href="/blog">
-          <h1 className="font-semibold underline decoration mb-4">
-            blog posts
-          </h1>
-        </a>
-        <div className="mb-8">
-          <BlogPost
-            href="/blog/cursorvsvscode"
-            title="Cursor vs Vscode"
-            date="22nd March, 2024"
-          />
-        </div>
-
         <h1 className="text-center font-semibold">projects</h1>
         <div className="mb-10">
           <Projects />
